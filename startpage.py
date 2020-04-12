3ea85cdae8b5b65221f3fe33e1ec53cdd6519609
lbelli2 <lucasbellido98@hotmail.com>
2020-04-12 13:19:30 -0400

    small thingy

--- startpage.py
+++ startpage.py
@@ -17,13 +17,13 @@ class StartPage(tk.Frame):
                             command=lambda: controller.show_frame("MemoriesPage"))
         button3 = tk.Button(self, text="View/Edit Users",
                             command=lambda: controller.show_frame("UsersPage"))
-        button4 = tk.Button(self, text="View/Edit Social Circles",
-                            command=lambda: controller.show_frame("SocialCirclePage"))
+        #button4 = tk.Button(self, text="View/Edit Social Circles",
+                           # command=lambda: controller.show_frame("SocialCirclePage"))
         
         exitButton = tk.Button(self, text="Exit Peacefully",
                                command=lambda: self.quit())
         AlbumsPageButton.pack()
         button2.pack()
         button3.pack()
-        button4.pack()
+        #button4.pack()
         exitButton.pack()
